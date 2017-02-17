d02adb3e9647e451280a1387111f5bdf49c9e302
theajack <1506282385@qq.com>
2017-02-17 17:44:41 +0800

    17-2-17

---
+++ assets/js/main.js
@@ -0,0 +1,200 @@
+var height,width,cx,cy,px=1,py=1;
+var maze=[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1],[1,0,1,0,1,1,0,1,0,1,1,1,0,1,0,0,0,1,0,1,1,1,0,1],[1,0,1,0,1,0,0,1,0,0,0,1,0,1,0,1,0,1,1,1,0,1,0,1],[1,0,1,0,1,1,1,1,1,0,1,1,0,1,1,1,0,1,0,1,0,1,0,1],[1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],[1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1],[1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,3,1],[1,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,1,1,1,0,1,0,1,1],[1,1,1,1,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0,0,1,0,0,1],[1,0,0,0,0,1,0,0,1,0,0,1,1,0,1,1,1,1,1,1,1,1,0,1],[1,0,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1],[1,0,1,1,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,1],[1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,0,0,1],[1,1,1,1,0,1,0,0,0,1,0,1,0,1,1,1,1,0,1,0,1,1,0,1],[1,1,3,1,0,1,0,1,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,1],[1,0,0,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1],[1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,1],[1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,1],[1,0,1,1,0,0,0,0,0,1,0,1,0,0,1,0,1,0,1,0,1,0,1,1],[1,0,0,0,0,1,0,1,0,0,0,0,0,1,1,0,1,0,1,0,1,1,1,1],[1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1,0,1,0,0,0,0,1],[1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,1,2,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
+//var maze=[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
+var map=J.id("map"),player=J.id("player"),blocks;
+var len,size=maze.length,viewSize=1.5;//config
+var mapLen,viewLen;
+var starNum=0;
+var isDown=false;
+var t;
+J.ready(function(){
+  /*canvas=J.id("gameCanvas");
+  height=J.height()-20;
+  canvas.width=height;
+  canvas.height=height;
+  geneMaze();*/
+  setSize();
+  createMaze();
+  J.class("button").event({
+    "onmousedown":function(){
+      var dirc=this.attr("id");
+      checkDirc(dirc);
+      var time=0;
+      t=setInterval(function(){
+        time+=10;
+        if(time>100){
+          checkDirc(dirc);
+        }
+      },10);
+    },
+    "onmouseup":function(){
+      clearInterval(t);
+    },
+    "onmouseout":function(){
+      clearInterval(t);
+    }
+  })
+});
+function test(){
+  
+}
+function setSize(){
+  height=J.height(),width=J.width(),cx=width/2,cy=height/2;
+  len=Math.round(((width>height)?height:width)*2/9);
+  mapLen=len*size;
+  viewLen=viewSize*2*len;
+  if(Jet.isMobile()){
+    var bwWidth=(width>height)?width/3:width*0.8;
+    var blen=Math.round(bwWidth/3);
+    bwWidth=blen*3;
+    bwHeight=blen*2;
+    J.id("btnBigWrapper").css("display","block");
+    J.id("btnWrapper").css({
+      width:bwWidth+"px",
+      height:bwHeight+"px"
+    });
+    J.class("fill").css({
+      width:(blen-4)+"px",
+      height:(blen-4)+"px"
+    });
+    J.class("button").css({
+      width:(blen-4)+"px",
+      height:(blen-4)+"px"
+    });
+  }else{
+    J.id("btnBigWrapper").css("display","none");
+  }
+  J.id("view").css({
+    left:(cx-viewLen/2)+"px",
+    top:(cy-viewLen/2)+"px",
+    height:viewLen+"px",
+    width:viewLen+"px"
+  });
+  player.css({
+    left:((viewLen-len)/2)+"px",
+    top:((viewLen-len)/2)+"px",
+    height:len+"px",
+    width:len+"px"
+  });
+  map.css({
+    height:mapLen+"px",
+    width:mapLen+"px",
+    left:(viewLen/2-px*len-len/2)+"px",
+    top:(viewLen/2-py*len-len/2)+"px"
+  });
+  J.id("starWrapper").css({
+    left:(cx+viewLen/2-150)+"px",
+    top:0+"px"
+  });
+}
+function checkDirc(dirc){
+  switch(dirc){
+    case "left":checkLeft();break;//left
+    case "up":checkUp();break;//up
+    case "right":checkRight();break;//right
+    case "down":checkDown();break;//down
+    default:break;
+  }
+}
+function checkHide(x,y){
+  
+}
+function moveMap(){
+  map.css({
+    left:(viewLen/2-px*len-len/2)+"px",
+    top:(viewLen/2-py*len-len/2)+"px"
+  });
+  var item=maze[py][px];
+  if(item==3){
+    blocks[py*size+px].replaceClass("star","road");
+    maze[py][px]=0;
+    J.id("starWrapper").child(starNum).addClass("get");
+    starNum++;
+  }else if(item==2&&starNum==3){
+    Jet.showWait("You win!","success");
+  }
+}
+function checkWin(){ 
+  if(blocks[py*size+px]==2&&starNum==3){
+    return true;
+  }
+  return false;
+}
+function createMaze(){ 
+  maze.each(function(a){
+    a.each(function(item){
+      var name="";
+      switch(item){
+        case 0:name="road";break;
+        case 1:name="wall";break;
+        case 2:name="target";break;
+        case 3:name="star";break;
+        default:break;
+      }
+      map.append(J.new("div").addClass(name).css({
+        height:len+"px",
+        width:len+"px"
+      }));
+    })
+  });
+  blocks=map.child();
+}
+function createRoad(){
+  
+}
+function openBranch(){
+  
+}
+function splitMaze(){
+  
+}
+document.onkeydown=function(e){
+  switch(e.keyCode){
+    case 37:checkLeft();break;//left
+    case 38:checkUp();break;//up
+    case 39:checkRight();break;//right
+    case 40:checkDown();break;//down
+    default:break;
+  }
+}
+function checkLeft(){
+  if(maze[py][px-1]!=1){
+    px--;
+    moveMap();
+  }
+}
+function checkRight(){
+  if(maze[py][px+1]!=1){
+    px++;
+    moveMap();
+  }
+}
+function checkUp(){
+  if(maze[py-1][px]!=1){
+    py--;
+    moveMap();
+  }
+}
+function checkDown(){
+  if(maze[py+1][px]!=1){
+    py++;
+    moveMap();
+  }
+}
+function test(){
+  px++;
+  moveMap();
+}
+window.onresize=function(){
+  setSize();
+  setBlockSize();
+}
+function setBlockSize(){
+  var names=["road","wall","target","star"];
+  names.each(function(item){
+    J.class(item).css({
+      height:len+"px",
+      width:len+"px"
+    });
+  });
+}
---
+++ assets/js/makeMaze.js
@@ -0,0 +1,214 @@
+var height=J.height(),width=J.width(),cx=width/2,cy=height/2,px=1,py=1;
+var maze,map=J.id("map"),blocks,current;
+
+var mapLen=height-20;
+var size=24;//config
+var len=Math.round(mapLen/size);
+var sx,sy;
+J.ready(function(){
+  initVara();
+  createMaze();
+});
+function initVara(){
+  mapLen=size*len;
+  sx=(cx-mapLen/2);
+  sy=(cy-mapLen/2);
+  current=J.id("current");
+  map.css({
+    height:mapLen+"px",
+    width:mapLen+"px",
+    left:(cx-mapLen/2)+"px",
+    top:(cy-mapLen/2)+"px"
+  });
+  current.css({
+    height:len+"px",
+    width:len+"px",
+    left:(sx+px*len)+"px",
+    top:(sy+py*len)+"px"
+  });
+  map.event("onclick","clickMap(event)");
+}
+function clickMap(e){
+  var x=Math.floor((e.clientX-(cx-mapLen/2))/len);
+  var y=Math.floor((e.clientY-(cy-mapLen/2))/len);
+  if(block==b_road){
+    if(x>px){
+      for(var i=1;i<=x-px;i++){
+        setRoad(px+i,py);
+      }
+    }else if(x<px){
+      for(var i=1;i<=px-x;i++){
+        setRoad(px-i,py);
+      }
+    }
+    px=x;
+    if(y>py){
+      for(var i=1;i<=y-py;i++){
+        setRoad(px,py+i);
+      }
+    }else if(y<py){
+      for(var i=1;i<=py-y;i++){
+        setRoad(px,py-i);
+      }
+    }
+    py=y;
+  }else if(block==b_wall){
+    px=x;
+    py=y;
+    maze[py][px]=b_wall;
+    blocks[y*size+x].replaceClass("road","wall");
+  }
+  setCurrent();
+}
+function printMaze(){
+  var s="["  
+  maze.each(function(a,i){
+    if(i>0){
+      s+=","
+    }
+    s+="["
+    a.each(function(item,j){
+      if(j>0){
+        s+=","
+      }
+      s+=item;
+    })
+    s+="]"
+  });
+  s+="]";
+}
+function createMaze(){
+  maze=new Array();
+  for(var i=0;i<size;i++){
+    maze[i]=new Array();
+    for(var j=0;j<size;j++){
+      map.append(J.new("div").addClass("wall").css({
+        height:len+"px",
+        width:len+"px"
+      }));
+      maze[i][j]=1;
+    }
+  }
+  
+  blocks=map.child();
+  setRoad(1,1);
+}
+var dirc=-1;
+var block=0;
+var step_len=-1;
+var step_min=2;
+var step_max=5;
+var step_num=0;
+var px=1,py=1;
+var d_up=1,d_down=2,d_left=3,d_right=4;
+var b_road=0,b_wall=1,b_target=2,b_star=3,b_player=4;
+function createRoad(){
+  while (step_num<10){
+    block=b_road;
+    var temp=dirc;
+    var t_min,t_max;
+    if(temp<3){
+      t_min=3;
+      t_max=4;
+    }else{
+      t_min=1;
+      t_max=2;
+    }
+    dirc=Jet.getRandom(t_min,t_max);
+    while(!checkDirc(step_min)){
+      dirc=Jet.getRandom(t_min,t_max);
+    }
+    step_len=Jet.getRandom(step_min,step_max);
+    while(!checkDirc(step_len)){
+      step_len=Jet.getRandom(step_min,step_max);
+    }
+    
+    switch(dirc){
+      case d_up:
+        for(var i=0;i<=step_len;i++){
+          setRoad(px,py-i);
+        }
+        py-=step_len;break;
+      case d_down:
+        for(var i=0;i<=step_len;i++){
+          setRoad(px,py+i);
+        }
+        py+=step_len;break;
+      case d_left:
+        for(var i=0;i<=step_len;i++){
+          setRoad(px-i,py);
+        }
+        px-=step_len;break;
+      case d_right:
+        for(var i=0;i<=step_len;i++){
+          setRoad(px+i,py);
+        }
+        px+=step_len;break;
+      default:break;
+    }
+    step_num++;
+    createRoad();
+  }
+}
+function setRoad(x,y){
+  maze[y][x]=b_road;
+  blocks[y*size+x].replaceClass("wall","road");
+}
+function setCurrent(){
+  current.css({
+    left:(sx+px*len)+"px",
+    top:(sy+py*len)+"px"
+  });
+}
+function checkDirc(len){
+  var result=true;
+  switch(dirc){
+    case d_up:if(len>=py)result=false;break;
+    case d_down:if(size-len<=py)result=false;break;
+    case d_left:if(len>=px)result=false;break;
+    case d_right:if(size-len<=px)result=false;break;
+    default:break;
+  }
+  return result;
+}
+function openBranch(){
+  
+}
+function splitMaze(){
+  
+}
+document.onkeydown=function(e){
+  switch(e.keyCode){
+    case 48:block=b_road;break;
+    case 49:block=b_wall;break;
+    case 50:block=b_target;break;
+    case 51:block=b_star;break;
+    case 52:block=b_player;break;
+    case 53:step_num=0;createRoad();break;
+    case 37:left();break;//left
+    case 38:up();break;//up
+    case 39:right();break;//right
+    case 40:down();break;//down
+    default:break;
+  }
+}
+function up(){
+  py--;
+  setRoad(px,py);
+  setCurrent();
+}
+function down(){
+  py++;
+  setRoad(px,py);
+  setCurrent();
+}
+function left(){
+  px--;
+  setRoad(px,py);
+  setCurrent();
+}
+function right(){
+  px++;
+  setRoad(px,py);
+  setCurrent();
+}
